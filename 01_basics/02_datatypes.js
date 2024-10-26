cf1bb2524acda529568c2855e3774d40b141b8d0
Om2807 <omp95043@gmail.com>
2024-10-26 03:47:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) //we are using node js not a browser
+
+//code readability is must
+
+let name = "om"
+let age = 19
+let isloggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint  =>
+//string => ""
+//boolean => true/false
+// null => satndalone value
+// undefined =>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "OM");
+console.log(typeof null);
