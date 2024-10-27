5bfcef0290f02828b95fa2f70ea41a315e86e37a
Om2807 <omp95043@gmail.com>
2024-10-27 04:45:06 +0000

    dataypoes summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const unotherId = Symbol('123')
+
+console.log(id === unotherId)
+
+
+// const bigNumber = 23165456476456465478864n
+
+
+
+// const score:number = 100 //type Script
+
+//refrence type (non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "om",
+    age: 20,
+}
+
+
+const myfunction = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof heros);
+
+/* https://262.ecma-international.org/5.1/#sec-11.4.3*/
