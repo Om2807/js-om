5892d9300e630fb72b73a043c74cdb2763f37c58
Om2807 <omp95043@gmail.com>
2024-12-19 04:50:07 +0000

    02

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -24,3 +24,8 @@ console.log(Array.isArray("om"))
 console.log(Array.from("om"))
 console.log(Array.from({name:"om"})) //intresting
 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
