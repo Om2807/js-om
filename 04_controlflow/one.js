ee29cca5815f9dcd2aa604c381bc2cfbfa566ba5
Om2807 <omp95043@gmail.com>
2024-12-27 06:25:51 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,15 @@
+// IF
+const isUserLOggedin = true
+const temprature = 41
+
+// if(temprature ===41){
+//     console.log("less tahn 50");
+// }
+// else{
+//     console.log("temprature is greaterthan 50");
+//     // console.log("not executde");
+// }
+// console.log("execute");
+
+
+// <, >, <=, >=, ==, !=, ===
