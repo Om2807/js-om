9197ed00ba0e0f78ef6c4ed43a252e1623fb8a30
Om2807 <omp95043@gmail.com>
2024-12-28 06:14:46 +0000

    one

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -13,3 +13,41 @@ const temprature = 41
 
 
 // <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if(balance < 750) {
+//     console.log("less than 750");
+// }  else if (balance < 900){
+//     console.log("less than 900");
+    
+// } else{
+//     console.log("less than 1200");
+// }
+
+const userLOggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if(userLOggedin && debitcard && 2==2){
+    console.log("allow to buy a cources");
+}
+
+if (loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in");
+}
+
