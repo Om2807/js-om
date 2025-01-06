b1aafc5186715fe255de7c4b3c00e2c4ad5b4869
Om2807 <omp95043@gmail.com>
2025-01-06 05:39:59 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const moth = "march"
+
+switch (moth) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("FEB");
+        break;
+    case "march":
+        console.log("MARCH");
+        break; 
+    case 4:
+        console.log("APRIL");
+        break;
+    case 5:
+        console.log("MAY");
+        break;
+    case 6:
+        console.log("jun");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("AUG");
+        break;
+    case 9:
+        console.log("SEP");
+        break;
+    case 10:
+        console.log("AUC");
+        break;
+    case 11:
+        console.log("NOV");
+        break;
+    case 12:
+        console.log("DEC");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
