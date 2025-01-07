22d6e9a06336c2210ad12439be0ff70d3c748bdc
Om2807 <omp95043@gmail.com>
2025-01-07 05:47:56 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,41 @@
+const coding = ["js","ruby","java","py","cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item,index,arr);
+// })
+
+const mycoding = [
+    {
+        languagename: "javascript",
+        languagefile: "js"
+    },
+    {
+        languagename: "java",
+        languagefile: "java"
+    },
+    {
+        languagename: "python",
+        languagefile: "py"
+    }
+]
+
+mycoding.forEach( (item) => {
+
+    // console.log(item.languagename);
+    // console.log(item.languagefile);
+    
+} )
