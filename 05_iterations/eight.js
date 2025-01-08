938297e4b0a491b85da30480e032f09374686ae4
Om2807 <omp95043@gmail.com>
2025-01-08 06:57:05 +0000

    eight

---
+++ 05_iterations/eight.js
@@ -0,0 +1,33 @@
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+    
+//     return acc + currval
+// }, 3)
+
+const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0 )
+
+// console.log(myTotal);
+
+const shoppingcart = [
+    {
+        itemname: "js course",
+        price: 2999
+    },
+    {
+        itemname: "py course",
+        price: 999
+    },
+    {
+        itemname: "mobiledeveloping course",
+        price: 5999
+    },
+    {
+        itemname: "datascience course",
+        price: 12999
+    },
+]
+
+const pricetopay = shoppingcart.reduce( (acc,item) => acc + item.price, 0)
+console.log(pricetopay);
